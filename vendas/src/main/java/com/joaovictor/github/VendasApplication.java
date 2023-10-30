81639c5ea700734cd7a3b48a3f16d80a4204176e
joao victor <joaovictormdasilva676@gmail.com>
2023-10-30 17:38:00 -0300

    novo projeto web(JSP) com Tomcant Apache 8.5.83

--- vendas/src/main/java/com/joaovictor/github/VendasApplication.java
+++ vendas/src/main/java/com/joaovictor/github/VendasApplication.java
@@ -2,6 +2,8 @@ package com.joaovictor.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VendasApplication {
