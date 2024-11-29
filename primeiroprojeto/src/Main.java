81f173b6fd9239dbe69e888ce331b74c29576c42
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-11-28 21:04:25 -0300

    Construindo o catálogo de filmes

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,8 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
-        System.out.println("Tudo bem?");
-        Soma.soma(10, 5);
-        Subtracao.subtracao(10, 5);
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top´Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        System.out.println("Esse filme: Top´Gun: Maverick, está incluso no plano? " + incluidoNoPlano);
+
+        float notaFilme = 8.1f;
+        System.out.println("O valor do filme: " + notaFilme);
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println("Média: " + media);
+
+        String sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println("Sinopse: " + sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
+        Media.calcularMedia(5, 9);
+        Desconto.calcularDesconto();
     }
 }
---
+++ primeiroprojeto/src/Media.java
@@ -0,0 +1,5 @@
+public class Media {
+    static void calcularMedia(double nota1, double nota2) {
+        System.out.println((int) ((nota1 + nota2) / 2));
+    }
+}
