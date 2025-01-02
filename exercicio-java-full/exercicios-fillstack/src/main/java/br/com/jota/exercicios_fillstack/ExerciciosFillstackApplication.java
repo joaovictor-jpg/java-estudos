f5a017e1a1ea27c261043655ae513bf630afde22
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-01-02 17:19:04 -0300

    exercícios projeto full-stack backend

---
+++ exercicio-java-full/exercicios-fillstack/src/main/java/br/com/jota/exercicios_fillstack/ExerciciosFillstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.jota.exercicios_fillstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosFillstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosFillstackApplication.class, args);
+	}
+
+}
---
+++ exercicio-java-full/exercicios-fillstack/src/test/java/br/com/jota/exercicios_fillstack/ExerciciosFillstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jota.exercicios_fillstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosFillstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
