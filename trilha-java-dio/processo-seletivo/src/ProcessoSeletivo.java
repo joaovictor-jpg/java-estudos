5fc5d69d15a950d21b9247055a9ae305ff01e0ff
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-02-03 13:42:25 -0300

    Seleção de candidatos

--- trilha-java-dio/processo-seletivo/src/ProcessoSeletivo.java
+++ trilha-java-dio/processo-seletivo/src/ProcessoSeletivo.java
@@ -1,18 +1,45 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
 
-    static void analisarCandidato(double salarioPretendido) {
+    static void selecaoCandidatos() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
+                "DANIEL", "JORGE" };
+
+        int candidatoSelecionador = 0;
+        int candidatoAtual = 0;
         double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("Ligar Para o candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Ligar para o candidato com contra proposta");
+
+        while (candidatoSelecionador < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato: " + candidato + ", solicitou este valor de salário " + salarioPretendido);
+
+            if (analisarCandidato(salarioPretendido, salarioBase)) {
+                System.out.println("O candidato: " + candidato + ", foi selecionado para a vaga");
+                candidatoSelecionador++;
+            } else {
+                System.out.println("O candidato: " + candidato + ", não foi selecionado para a vaga");
+            }
+
+            candidatoAtual++;
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static boolean analisarCandidato(double salarioPretendido, double salarioBase) {
+        if (salarioBase >= salarioPretendido) {
+            return true;
         } else {
-            System.out.println("Aguardando o resultado dos demais candidatos");
+            return false;
         }
     }
 }
