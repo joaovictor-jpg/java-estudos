4968dd8d00ec6c6a948c4609bba6708773e411d7
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-02-03 16:03:55 -0300

    Desafio 2

--- trilha-java-dio/Desafios/src/App.java
+++ trilha-java-dio/Desafios/src/App.java
@@ -1,42 +1,21 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Entrada do saldo inicial
-        double saldoInicial = scanner.nextDouble();
+        double saldo = scanner.nextDouble();
 
-        scanner.nextLine();
-
-        // TODO: Na linha abaixo, implemente a entrada das três transações:
-        double entradaDeValores = entradaDosValores(scanner);
-
-        // TODO: Na linha abaixo, realize o cálculo do saldo final:
-        double saldoFinal = extrato(saldoInicial, entradaDeValores);
-
-        // Saldo final
-        System.out.printf("%.2f\n", saldoFinal);
+        // TODO: Implemente a classificação do cliente com base no saldo:
+        
+        if(saldo < 0.00) {
+            System.out.println("Negativado");
+        } else if(saldo <= 500.0) {
+            System.out.println("Baixo");
+        } else {
+            System.out.println("Confortavel");
+        }
 
         scanner.close();
-    }
-
-    private static double extrato(double saldoInicial, double entradaDeValores) {
-        if (entradaDeValores < 0) {
-            return saldoInicial + entradaDeValores;
-        } else if (saldoInicial == 0) {
-            return entradaDeValores;
-        } else {
-            return saldoInicial - entradaDeValores;
-        }
-    }
-
-    private static double entradaDosValores(Scanner scanner) {
-        double valor = 0.0;
-        for (int i = 0; i < 3; i++) {
-            valor += scanner.nextDouble();
-        }
-
-        return valor;
     }
 }
