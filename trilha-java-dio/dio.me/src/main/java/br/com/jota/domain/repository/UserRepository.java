627aa1bc38c055a17a6cd3cf7f69c3d154989fa8
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-02-21 19:32:38 -0300

    Implementando a camada de dominio

---
+++ trilha-java-dio/dio.me/src/main/java/br/com/jota/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.jota.domain.repository;
+
+import br.com.jota.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
