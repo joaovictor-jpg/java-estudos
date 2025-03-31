552ed2d622178d10ea64e698403d92e26808302c
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-03-31 20:27:06 -0300

    refatorando gitignore

---
+++ boasPraticas/3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -0,0 +1,44 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.services.AdocaoServices;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/adocoes")
+public class AdocaoController {
+
+    @Autowired
+    private AdocaoServices adocaoServices;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<String> solicitar(@RequestBody @Valid Adocao adocao) {
+        try {
+            adocaoServices.solicitar(adocao);
+            return ResponseEntity.ok().build();
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/aprovar")
+    @Transactional
+    public ResponseEntity<String> aprovar(@RequestBody @Valid Adocao adocao) {
+        adocaoServices.aprovar(adocao);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/reprovar")
+    @Transactional
+    public ResponseEntity<String> reprovar(@RequestBody @Valid Adocao adocao) {
+        adocaoServices.reprovar(adocao);
+        return ResponseEntity.ok().build();
+    }
+
+}
