8cf7a58502562f67808724913c650349c1facd8b
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-04-02 21:17:47 -0300

    Criando teste para a classe validação pet disponivel

---
+++ boasPraticas/3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -0,0 +1,52 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    ValidacaoPetDisponivel validacaoPetDisponivel;
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    @Description("Validação Pet Disponivel")
+    void validarCena1() {
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+        Assertions.assertDoesNotThrow(() -> validacaoPetDisponivel.validar(dto));
+
+    }
+
+    @Test
+    @Description("Deve lançar uma exeção de ValidacaoException quando pet está adotado")
+    void validarCena2() {
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        Assertions.assertThrows(ValidacaoException.class, () -> validacaoPetDisponivel.validar(dto));
+    }
+
+}
