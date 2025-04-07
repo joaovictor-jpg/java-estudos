7115ed82c0be109efcab4d03ace81bd63d63e689
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-04-06 23:50:12 -0300

    Trabalhando com as threads virtual

---
+++ boasPraticas/simulacao_banco-main/src/br/com/alura/banco/TesteMultiplasThreads.java
@@ -0,0 +1,27 @@
+package br.com.alura.banco;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TesteMultiplasThreads {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        Instant inicio = Instant.now();
+
+        try (ExecutorService e = executor){
+            for (int i = 1; i<=100000; i++){
+                var tarefa = new ExecutaTarefa();
+                e.execute(tarefa);
+            }
+        }
+
+        Instant fim = Instant.now();
+
+        Duration duration = Duration.between(inicio, fim);
+
+        System.out.println("Tempo gasto pelas tarefas: " + duration.getSeconds());
+    }
+}
